aa33fdc9cded93b64bbdf1d3ee4ee480d5be50f2
undefined <undefined@example.com>
2012-11-24 19:15:09 +0900

    練習問題2.(1) (2)>>=演算子を使った明示的なシーケンシング

--- simple-parser.hs
+++ simple-parser.hs
@@ -32,8 +32,7 @@ parseAtom = do first <- letter <|> symbol
                           _    -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = do x <- many1 digit
-                 return $ (Number . read) x
+parseNumber = many1 digit >>= return . Number . read
 
 parseString :: Parser LispVal
 parseString = do char '"'
