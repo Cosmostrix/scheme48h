3bdcd87b23741fda3b512024274cac9ab8cc04b9
undefined <undefined@example.com>
2012-11-24 17:24:49 +0900

    練習問題1.(2) 2つの引数に対して簡単な算術演算を行うようにプログラムを変更しなさい。文字列を数値に変換するにはreadが、数値を文字列に戻すにはshowが使えます。いくつか違う演算を行うよう試してみてください。

--- Hello.hs
+++ Hello.hs
@@ -5,6 +5,8 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0 ++ args !! 1)
+  let num1 = read (args !! 0)
+      num2 = read (args !! 1) in
+      putStrLn ("Hello, " ++ show (num1 + num2) ++ "," ++ show (num1 `div` num2))
 
--- ghc -o hello.exe --make Hello.hs && hello world! !!!!
+-- ghc -o hello.exe --make Hello.hs && hello 8 3
