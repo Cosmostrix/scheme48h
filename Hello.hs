9ca79075e4379938bc068386df9667caa7bb1ec2
undefined <undefined@example.com>
2012-11-24 17:27:29 +0900

    練習問題1.(3) getLineはコンソールから一行読み込み、文字列として返すIOアクションです。名前を入力を促し、名前を読み、コマンドライン引数の代わりにそれを出力するようにプログラムを変更しなさい。

--- Hello.hs
+++ Hello.hs
@@ -4,9 +4,8 @@ import System.Environment
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let num1 = read (args !! 0)
-      num2 = read (args !! 1) in
-      putStrLn ("Hello, " ++ show (num1 + num2) ++ "," ++ show (num1 `div` num2))
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
 
--- ghc -o hello.exe --make Hello.hs && hello 8 3
+-- ghc -o hello.exe --make Hello.hs && hello
+-- world!
