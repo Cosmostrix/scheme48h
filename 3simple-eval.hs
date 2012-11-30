4143eeed1ab09ce748022d8f59bff6b12d922d38
undefined <undefined@example.com>
2012-12-01 02:12:44 +0900

    [48時間でSchemeを書こう/評価: 第一部/基本的なプリミティブを加える]

--- 3simple-eval.hs
+++ 3simple-eval.hs
@@ -161,7 +161,7 @@ parseVector = do
 
 parseExpr :: Parser LispVal
 parseExpr = parseSharpSyntax
-        <|> parseNum
+        <|> try parseNum
         <|> parseAtom
         <|> parseString
         <|> parseQuoted <|> parseQusaiquote
@@ -201,9 +201,35 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+--eval v= String$show v
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [ ("+", numericBinop (+)),
+               ("-", numericBinop (-)),
+               ("*", numericBinop (*)),
+               ("/", numericBinop div),
+               ("mod", numericBinop mod),
+               ("quotient", numericBinop quot),
+               ("remainder", numericBinop rem)
+             ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                           if null parsed
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
 
 -- ghc -package parsec -o 3simple_eval.exe --make 3simple-eval.hs
--- 3simple_eval "'atom"
--- 3simple_eval 2
--- 3simple_eval "\"asdgh\""
 -- 3simple_eval "(+ 2 2)"
+-- 3simple_eval "(+ 2 (- 4 1))"
+-- 3simple_eval "(- (+ 4 6 3) 3 5 2)"
