db8d954c15ad62c511b09f5a8fb1d5b481f8aca3
undefined <undefined@example.com>
2012-12-02 16:05:12 +0900

    [48時間でSchemeを書こう/REPLの作成]

--- 6interactive.hs
+++ 6interactive.hs
@@ -8,12 +8,38 @@ import Data.Ratio
 import Data.Complex
 import Data.Array
 import Control.Monad.Error
+import System.IO
 
 main :: IO ()
 main = do
   args <- getArgs
-  result <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError result
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Program takes only 0 or 1 argument"
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $
+    trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+     then return ()
+     else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 data LispVal = Atom String
              | List [LispVal]
@@ -404,12 +430,10 @@ trapError action = catchError action (return . show)
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
 
--- ghc -package parsec -fglasgow-exts -o 5basic-eval.exe --make 5basic-eval.hs
--- 5basic-eval "(cdr '(a simple test))"
--- 5basic-eval "(car (cdr '(a simple test)))"
--- 5basic-eval "(car '((this is) a test))"
--- 5basic-eval "(cons '(this is) 'test)"
--- 5basic-eval "(cons '(this is) '())"
--- 5basic-eval "(eqv? 1 3)"
--- 5basic-eval "(eqv? 3 3)"
--- 5basic-eval "(eqv? 'atom 'atom)"
+-- ghc -package parsec -fglasgow-exts -o 6interactive.exe --make 6interactive.hs
+-- 6interactive.exe
+-- Lisp>>> (+ 2 3)
+-- Lisp>>> (cons this '())
+-- Lisp>>> (cons 2 3)
+-- Lisp>>> (cons 'this '())
+-- Lisp>>> quit
